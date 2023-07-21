2e045aa841d42c12229cd1dbc48e784c7901b267
plastikSun <marcom2k01@gmail.com>
2023-07-21 16:43:21 +0200

    complete: goal 1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
+let bfList = document.getElementById("FizzBuzzList")
+
+const fizz = "Fizz", buzz = "Buzz", fizzBuzz = "FizzBuzz";
+
 for (i = 1; i <= 100; i++) {
+    let emptyItem = document.createElement("li")
     
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        console.log(fizzBuzz)
+        emptyItem.innerHTML = fizzBuzz
+
     } else if (i % 3 == 0) {
-        console.log("Fizz")
+        console.log(fizz)
+        emptyItem.innerHTML = fizz
+
     } else if (i % 5 == 0) {
-        console.log("Buzz")
+        console.log(buzz)
+        emptyItem.innerHTML = buzz
+
     } else {
         console.log(i)
+        emptyItem.innerHTML = i
     }
 
+    bfList.append(emptyItem)
 
 }
