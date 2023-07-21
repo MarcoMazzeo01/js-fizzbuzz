34e5530d2b827d1e1bc6585a7bc4b52f7db64fc0
plastikSun <marcom2k01@gmail.com>
2023-07-21 18:30:39 +0200

    complete: goal 2 (added number badges too)

--- js/main.js
+++ js/main.js
@@ -6,9 +6,13 @@ emptyColTemplate.classList.add("col","centeringItem")
 const numberTemplate = document.createElement("div")
 numberTemplate.classList.add("numberItem")
 
-emptyColTemplate.append(numberTemplate)
+const badgeTemplate = document.createElement("span")
+badgeTemplate.classList.add("position-absolute","translate-middle", "badge", "rounded-pill","bg-danger")
 
+numberTemplate.append(badgeTemplate)
+emptyColTemplate.appendChild(numberTemplate)
 
+console.log(emptyColTemplate)
 const fizz = "Fizz", buzz = "Buzz", fizzBuzz = "FizzBuzz";
 
 
@@ -16,27 +20,31 @@ const fizz = "Fizz", buzz = "Buzz", fizzBuzz = "FizzBuzz";
 for (i = 1; i <= 100; i++) {
     let column = emptyColTemplate.cloneNode(true)
     let numberDiv = column.lastChild
+    let badge = numberDiv.lastChild
+    badge.innerHTML = i
     
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log(fizzBuzz)
-        numberDiv.innerHTML = fizzBuzz
+       console.log(fizzBuzz)
+        numberDiv.innerHTML += fizzBuzz
         numberDiv.style.backgroundColor = "#BC455A";
 
     } else if (i % 3 == 0) {
         console.log(fizz)
-        numberDiv.innerHTML = fizz
+        numberDiv.innerHTML += fizz
         numberDiv.style.backgroundColor = "#FDBA77";
 
     } else if (i % 5 == 0) {
         console.log(buzz)
-        numberDiv.innerHTML = buzz
+        numberDiv.innerHTML += buzz
         numberDiv.style.backgroundColor = "#F9D6D3";
 
     } else {
         console.log(i)
-        numberDiv.innerHTML = i
+        badge.classList.add("d-none")
+        numberDiv.innerHTML += i
+        console.log(badge)
     }
 
-    bfList.appendChild(column)
+    bfList.append(column)
 
 }
