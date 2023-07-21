27542805e2ce1ca9146ad2fff272a5b5ca89cb5e
plastikSun <marcom2k01@gmail.com>
2023-07-21 17:54:30 +0200

    fix: grid element sizing & alignment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let bfList = document.getElementById("FizzBuzzList")
 
 const emptyColTemplate = document.createElement("div")
-emptyColTemplate.classList.add("col")
+emptyColTemplate.classList.add("col","centeringItem")
 
 const numberTemplate = document.createElement("div")
 numberTemplate.classList.add("numberItem")
@@ -20,14 +20,17 @@ for (i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log(fizzBuzz)
         numberDiv.innerHTML = fizzBuzz
+        numberDiv.style.backgroundColor = "#BC455A";
 
     } else if (i % 3 == 0) {
         console.log(fizz)
         numberDiv.innerHTML = fizz
+        numberDiv.style.backgroundColor = "#FDBA77";
 
     } else if (i % 5 == 0) {
         console.log(buzz)
         numberDiv.innerHTML = buzz
+        numberDiv.style.backgroundColor = "#F9D6D3";
 
     } else {
         console.log(i)
