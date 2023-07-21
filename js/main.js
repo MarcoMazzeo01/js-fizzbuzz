5a336a5fbb7c4ac8b2ac436f93af002518a03344
plastikSun <marcom2k01@gmail.com>
2023-07-21 17:37:18 +0200

    add: grid layout

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 let bfList = document.getElementById("FizzBuzzList")
 
+const emptyColTemplate = document.createElement("div")
+emptyColTemplate.classList.add("col")
+
+const numberTemplate = document.createElement("div")
+numberTemplate.classList.add("numberItem")
+
+emptyColTemplate.append(numberTemplate)
+
+
 const fizz = "Fizz", buzz = "Buzz", fizzBuzz = "FizzBuzz";
 
+
+//main
 for (i = 1; i <= 100; i++) {
-    let emptyItem = document.createElement("li")
+    let column = emptyColTemplate.cloneNode(true)
+    let numberDiv = column.lastChild
     
     if (i % 3 == 0 && i % 5 == 0) {
         console.log(fizzBuzz)
-        emptyItem.innerHTML = fizzBuzz
+        numberDiv.innerHTML = fizzBuzz
 
     } else if (i % 3 == 0) {
         console.log(fizz)
-        emptyItem.innerHTML = fizz
+        numberDiv.innerHTML = fizz
 
     } else if (i % 5 == 0) {
         console.log(buzz)
-        emptyItem.innerHTML = buzz
+        numberDiv.innerHTML = buzz
 
     } else {
         console.log(i)
-        emptyItem.innerHTML = i
+        numberDiv.innerHTML = i
     }
 
-    bfList.append(emptyItem)
+    bfList.appendChild(column)
 
 }
